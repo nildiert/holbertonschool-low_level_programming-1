55ee9d449a59b3aa5e740c2301a0b72ae5ddf49b
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-01-24 06:23:07 +0000

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *  add - adds two integers and returns the result
+ * @a: interger input 1
+ * @b: integer input 2
+ * Return: c
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+
+}
