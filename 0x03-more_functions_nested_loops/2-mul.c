c14898ab010f49e7b26c603f42a78cdeb85c0aa1
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-01-26 01:29:08 +0000

    function to multiple two varibles

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * mul - multiples a and b
+ *
+ * @a: varible 1
+ * @b: varible 2
+ * Return: product of a & b
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+	return (c);
+}
