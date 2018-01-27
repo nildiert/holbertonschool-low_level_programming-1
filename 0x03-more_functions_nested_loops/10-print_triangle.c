c1c0c7daccc3f1a5ac1d876f6d10a2f824989a6b
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-01-27 02:34:53 +0000

    prints a triangle

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - print a triangle
+ *
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int line, column, spaces;
+
+	for (line = 1; line <= size; line++)
+	{
+		spaces = size - line; /* amount of spaces per line */
+
+		for (column = 0; column < size; column++)
+		{
+			if (column < spaces)
+			{
+				_putchar(' ');
+			}
+
+			else
+			{
+				_putchar('#');
+			}
+
+		}
+		_putchar('\n');
+	}
+
+}
