9eb3b8344081a0ddf6cefee4e3037023f3348e7a
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-01-31 00:34:11 +0000

    return the length of a string

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - length of a string
+ *
+ * @s: length of a string
+ *
+ * Return: returns length of a string
+ */
+
+int _strlen(char *s)
+{
+	int a; /* length */
+
+	for (a = 0; s[a] != '\0'; a++) /* string counter */
+
+	return (a);
+}
