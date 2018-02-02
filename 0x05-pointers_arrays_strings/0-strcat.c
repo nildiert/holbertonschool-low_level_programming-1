8e26497916bd7a23a22b223b2a9e1353f2a72bd4
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-02 06:47:02 +0000

    concatenates two strings

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+ /**
+  * _strcat - concatenates two strings
+  *
+  * @dest: string destination
+  *
+  * @src: string source
+  */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int src_len = 0;
+	int dest_len = 0;
+	int count;
+
+	for (count = 0; src[count] != '\0'; count++) /* find src len */
+		src_len++;
+
+	for (count = 0; dest[count] != '\0'; count++) /* find dest len */
+		dest_len++;
+
+	for (count = 0; count < src_len; count ++)
+		dest[dest_len + count] = src[count];
+
+	dest[dest_len + count] = '\0'; /* add null */
+
+		return(dest);
+}
