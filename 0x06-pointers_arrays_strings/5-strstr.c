558e46586ea1371a36d7e25107b0a75678d836b0
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-06 23:35:09 +0000

    locates a substring

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -11,18 +11,20 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i; /* counter */
-	int j;
+	int i = 0; /* counter */
+	int j = 0;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		for (j = 0; needle[j] == haystack[i]; j++)
 		{
-			if (haystack[j] == needle[i])
-			{
-				return (&needle[i]);
-			}
+			i++;
+			return (haystack + i);
 		}
 	}
+
+	if (*needle == '\0')
+		return (haystack);
+
 	return (0);
 }
