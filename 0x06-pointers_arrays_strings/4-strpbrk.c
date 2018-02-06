2b557ad6a5d195cba66ee3f9c3ce8ee3d0030dc6
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-06 06:37:56 +0000

    searches a string for any of a set of bytes

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ *
+ * @s: initial segement
+ * @accept: bytes
+ *
+ * Return: how many are matching
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i; /* counter */
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (0);
+}
