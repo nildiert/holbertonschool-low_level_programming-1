f55f7587554af9e7564ee34cd5afbaf203dd0885
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-06 00:01:14 +0000

    commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int a = 0;
+int b = 0;\
+\/etc/hosts
+oooo
---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * *leet - replaces 'aA,eE, oO, tT, l,L with 4,3,0,7,1
+ *
+ * @char - string input
+ *
+ * Return: char *
+ */
+
+char *leet(char *)
+{
+	x[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'}
+
+	y[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'}
+
+	int x[];
+	int y[];
+
+	while (str[index] != '\0')
+	{
+		while (str[index] != x[new_index] && x[new_index] != '\0')
+		{
+			new_index++;
+		}
+
+		if (new_index] != '\0');
+
+		str[index] = y[new_index]
+	}
+}
