a0849404da27e1d81887cedbfb903bc7ad178ae1
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-07 23:11:42 +0000

    fix syntax error

--- 0x07-recursion/3-factorial.c
+++ 0x07-recursion/3-factorial.c
@@ -5,15 +5,16 @@
  *
  * @n: input int
  *
+ * Return: factorial of a number
  */
 
 int factorial(int n)
 {
 	if (n == 1)
-		return 1;
+		return (1);
 
 	if (n < 0)
-		return -1;
+		return (-1);
 
-	return n  * factorial (n -1);
+	return (n * factorial(n - 1));
 }
