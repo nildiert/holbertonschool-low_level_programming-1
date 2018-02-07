43a23118e2e9a3aa0aaaf4a53ad8f78c0ffe019b
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-07 04:46:06 +0000

    copies a string from src to destination

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copies a string from src to dest
+ *
+ * @dest: source of data
+ * @src: destination for data
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+	return(dest);
+
+}
