be6b8914835caf6eb153b67bb2f623e6902da99a
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-08 04:08:39 +0000

    returns the length of a string

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: input string
+ *
+ * Return: length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return(_strlen_recursion(s + 1) + 1);
+	/* outside +1 is accounted for after all recursions */
+}
