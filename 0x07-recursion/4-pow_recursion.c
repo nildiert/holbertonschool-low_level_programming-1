a5245ca5ad7a6197376b2a8d7b5e4e6a52a5e320
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-08 06:19:22 +0000

    fix edge cases

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _pow_recusion - returns value of x raised to the power of y
+ * _pow_recursion - returns value of x raised to the power of y
  *
  * @x: int value
  * @y: int value
@@ -11,11 +11,11 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 1)
-		return (1);
-
 	if (y < 0)
 		return (-1);
+
+	if (y == 0)
+		return (1);
 
 	return (x * _pow_recursion(x, y - 1));
 }
