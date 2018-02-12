3d8551ea710ab9c90155cafb39297880683b014b
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-12 05:32:12 +0000

    prints the program's name

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of this program
+ *
+ * @argc: argument count
+ * @argv: array of strings
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
