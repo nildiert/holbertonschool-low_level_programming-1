7511049277099083180b55f999a74cb19e8ad0fa
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-13 05:27:49 +0000

    capitalizes all words of a string

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes all worlds of a string
+ *
+ * @char - string input
+ *
+ * Return: 0
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+	int j;
+	int sp[] = {' ','\n','\t',',',';','.','!','?','"','(',')','{','}','\0'};
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; sp[j] != '\0'; j++)
+		{
+			if (s[i] == sp[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] -= 32;
+			}
+		}
+	}
+	return (s);
+}
