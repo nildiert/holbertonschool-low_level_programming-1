55367cb2b507f1eba1a2b97d037b20342fbb3400
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-16 18:54:42 +0000

    seperate arguments with a new line

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - seperate all arguments in new lines
+ *
+ * @ac: arg count
+ * @av: arguments
+ *
+ * Return: null or string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i; /* counter */
+	int j; /* counter 2 */
+	int k = 0; /* counter 3 */
+	int tot; /* total*/
+	char *m;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++);
+		{
+			tot++;
+		}
+
+		tot++;
+	}
+	tot++;
+	m = malloc(sizeof(char) * (tot));
+
+	if (m == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			m[k] = av[i][j];
+			k++;
+		}
+		m[k] = '\n';
+		k++;
+	}
+
+	m[k] = '\0';
+
+	return (m);
+}
