39e0150c76a135c1e9851fada312db5f0d63fde9
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-16 07:18:28 +0000

    allocates memory for an array, using malloc

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array of nmemb elements of size bytes
+ *
+ * @nmemb: number of member inputs
+ * @size: input size
+ *
+ * Return: array
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *k; /* pointer of any type */
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	k = malloc(nmemb * size);
+
+	if (k  == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		*((char*) k + i) = 0; /* fills in 1 byte at a time w char */
+
+	return (k);
+}
