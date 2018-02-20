dabd0dc5782aca9e3d6e8cf3ba6b6797f0ceb8df
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-20 07:35:30 +0000

    struct dog info

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - print dog info
+ *
+ * @d: dog info
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("\n");
+
+	if (d->name == NULL)
+		printf("Name: (nil)");
+
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: nil");
+
+	else
+		printf("Owner: %s\n", d->owner);
+}
