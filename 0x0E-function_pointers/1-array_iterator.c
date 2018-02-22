28907fbfd8e8608ab4ea5ebbc3baf326a8315cac
[vmdn23] <[victormdnguyen88@gmail.com]>
2018-02-22 02:52:08 +0000

    changed to size_t for iterator

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -15,7 +15,14 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
+	size_t i = 0;
+
+	if (!array)
+		return;
+        if (size <= 0)
+		return;
+	if (!action)
+		return;
 
 	while (i < size)
 	{
