6d5d3ad91f8456dae5c347193728835d6fb48c36
vmdn23 <victormdnguyen88@gmail.com>
2018-02-24 22:04:28 +0000

    change = to +=

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -17,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(vlist, n);
 
 	for (i = 0; i < n; i++)
-		sum = va_arg(vlist, unsigned int);
+		sum += va_arg(vlist, unsigned int);
 
 	va_end(vlist);
 	return (sum);
