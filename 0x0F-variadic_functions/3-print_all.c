1d778201a11d848d1e0bc0ba220ee879eacbb432
vmdn23 <victormdnguyen88@gmail.com>
2018-02-25 06:16:37 +0000

    prints anything

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,91 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_c - prints characters
+ * @risuto: list
+ * Return: Always 0.
+ */
+
+void print_c(va_list risuto)
+{
+	printf("%c", va_arg(risuto, int));
+}
+
+/**
+ * print_i - prints integers
+ * @risuto: list
+ * Return: Always 0.
+ */
+
+void print_i(va_list risuto)
+{
+	printf("%d", va_arg(risuto, int));
+}
+
+/**
+ * print_f - prints floating ints
+ * @risuto: list
+ * Return: Always 0.
+ */
+
+void print_f(va_list risuto)
+{
+	printf("%f", va_arg(risuto, double));
+}
+
+/**
+ * print_s - prints a string
+ * @risuto: list
+ * Return: Always 0.
+ */
+
+void print_s(va_list risuto)
+{
+	char *str;
+
+	str = va_arg(risuto, char*);
+	printf("%s", str);
+}
+
+/**
+ * print_all - print all types of input
+ * @format: input
+ * Return: Always 0.
+ */
+
+
+void print_all(const char * const format, ...)
+{
+	int i, j;
+	char *b = ""; /* blank */
+	char *cs = ", "; /* comma space */
+	va_list risuto;
+	pt_t types[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"f", print_f},
+		{"s", print_s},
+		{NULL, NULL},
+	};
+
+	va_start(risuto, format);
+
+	i = 0;
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (types[j].ro != NULL)
+		{
+			if (format[i] == *(types[j].ro))
+			{
+				printf("%s", m);
+				(types[j].f)(risuto);
+				m = n; /* reassign to separator */
+			}
+		j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(risuto);
+}
