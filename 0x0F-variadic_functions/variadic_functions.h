7ef3fc4f19337d2b3fdc01e2536003155276415c
vmdn23 <victormdnguyen88@gmail.com>
2018-02-26 18:21:44 +0000

    betty

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -10,14 +10,21 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-typedef struct print_type {
+/**
+ * struct print_type - structure for different types
+ * @ro: formatted character c,i,f,s
+ * @f: function pointer to list
+ *
+ */
+typedef struct print_type
+{
 	char *ro;
 	void (*f)(va_list); /* function pointer */
-}pt_t;
+} pt_t;
 
-void print_c (va_list risuto);
-void print_i (va_list risuto);
-void print_f (va_list risuto);
-void print_s (va_list risuto);
+void print_c(va_list risuto);
+void print_i(va_list risuto);
+void print_f(va_list risuto);
+void print_s(va_list risuto);
 
 #endif
