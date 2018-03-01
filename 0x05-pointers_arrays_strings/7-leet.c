4b58afe9a3e7d5810bfb99abcdab241b6d2aba7b
vmdn23 <victormdnguyen88@gmail.com>
2018-03-01 00:01:54 +0000

    account for edge case

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * *leet - replaces 'aA,eE, oO, tT, l,L with 4,3,0,7,1
@@ -10,23 +11,23 @@
 
 char *leet(char *s)
 {
-	int x[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char *x = "aAeEoOtTlL";
 
-	int y[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	char *y = "4433007711";
 
 	int i = 0;
 	int j;
 
 	while (s[i] != '\0')
 	{
-		for (j = 0; x[j]; j++)
+		for (j = 0; x[j] != '\0'; j++)
 		{
 			if (x[j] == s[i])
+			{
 				s[i] = y[j];
+			}
 		}
-
 		i++;
 	}
-
 	return (s);
 }
