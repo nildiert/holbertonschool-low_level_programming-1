90aef787ac92e7265acc549e3f5fb9e18d9c4b84
vmdn23 <victormdnguyen88@gmail.com>
2018-03-02 05:26:29 +0000

    free linked list

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list -free linked list
+ *
+ * @head: start of linked list
+ *
+ * Return: address
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
