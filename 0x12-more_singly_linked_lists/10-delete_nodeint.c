da258ae2beb95d25c3bb79faebe557cfad3d9627
vmdn23 <victormdnguyen88@gmail.com>
2018-03-07 22:47:01 +0000

    add a comment for if > index

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	counter = 0;
 	while (counter < index - 1)
 	{
-		if (temp == NULL)
+		if (temp == NULL) /* accounts for if input is > index */
 			return (-1);
 
 		temp = temp->next;
