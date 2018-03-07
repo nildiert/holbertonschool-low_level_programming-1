6be5bc4ccbfda5661e289a63369b5b86ca78ea09
vmdn23 <victormdnguyen88@gmail.com>
2018-03-07 04:26:40 +0000

    account for *head, -1 edge cases

---
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index of a linked list
+ * @head: pointer to a pointer
+ * Return: 1 if successful or -1 if fails
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp;
+	listint_t *new_node;
+	unsigned int counter;
+
+	temp = *head;
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+
+	counter = 0;
+	while (counter < index - 1)
+	{
+		if (temp == NULL)
+			return (-1);
+
+		temp = temp->next;
+		counter++;
+	}
+
+	new_node = (temp->next)->next;
+	free(temp->next);
+	temp->next = new_node;
+	return (1);
+}
