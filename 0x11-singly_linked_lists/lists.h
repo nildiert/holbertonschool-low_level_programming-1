6194a56481a244c46301edd5c22500ac20d880fa
vmdn23 <victormdnguyen88@gmail.com>
2018-03-07 22:48:00 +0000

    re add

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -14,6 +14,7 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
+
 typedef struct list_s
 {
 	char *str;
@@ -25,4 +26,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void beatmain(void) __attribute__ ((constructor));
 #endif
