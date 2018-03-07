c1138fa52cb31e4cc057483629180f3ac20ffc68
vmdn23 <victormdnguyen88@gmail.com>
2018-03-07 22:22:06 +0000

    execute before main

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * beatmain - prints something before main
+ *
+ * Return: nothing
+ */
+
+void beatmain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
