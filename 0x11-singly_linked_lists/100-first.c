fce3fac15cd24cb9632c5e70d18fee51d980e66d
vmdn23 <victormdnguyen88@gmail.com>
2018-03-07 22:25:53 +0000

    fix prototype

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
  * Return: nothing
  */
 
-void beatmain(void)
+void beatmain(__attribute__ ((constructor)))
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
