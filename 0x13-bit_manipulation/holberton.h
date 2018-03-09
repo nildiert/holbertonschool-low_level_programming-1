adbb039929dd748bfbfcdc6247e5dbc90ca62736
vmdn23 <victormdnguyen88@gmail.com>
2018-03-09 04:55:58 +0000

    update header file

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -1,21 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <stdlib.h>
-
 #include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
