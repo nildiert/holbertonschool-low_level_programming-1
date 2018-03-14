4fab290b6c1b55fee129fe3c55fe6a1dd248fa6a
vmdn23 <victormdnguyen88@gmail.com>
2018-03-14 00:46:16 +0000

    reupload

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filenanme: name of the file
+ * @letters: letters to be read
+ * Return - actul number of letters it could read and print
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t bytes_read, bytes_written;
@@ -14,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buf = malloc((sizeof(char) * letters) + 1); /* space for \0 */
+	buf = malloc(sizeof(char) * letters); /* space for \0 */
 	if (buf == NULL)
 		return (0);
 
@@ -23,7 +30,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (bytes_read == -1)
 		return (0);
 
-	buf[bytes_read + 1] = '\0'; /* off set value first */
+	buf[bytes_read] = '\0'; /* off set value first */
 
 	close(fd);
 
