1092da04f21b991448528c45e3d88bce849a49b7
vmdn23 <victormdnguyen88@gmail.com>
2018-03-14 00:58:29 +0000

    betty

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -2,9 +2,9 @@
 
 /**
  * read_textfile - reads a text file and prints it to the standard output
- * @filenanme: name of the file
+ * @filename: name of the file
  * @letters: letters to be read
- * Return - actul number of letters it could read and print
+ * Return: actual number of letters it could read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -13,12 +13,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buf;
 
-	if (filename == NULL)
+	if (filename == NULL) /* checks if there is anything to read */
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY); /* read the file only */
 
-	if (fd == -1)
+	if (fd == -1) /* if fail read, write, open == -1 */
 		return (0);
 
 	buf = malloc(sizeof(char) * letters); /* space for \0 */
@@ -26,15 +26,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	bytes_read = read(fd, buf, letters);
+	/* reading into fd based on letters, of size buff */
 
 	if (bytes_read == -1)
 		return (0);
 
-	buf[bytes_read] = '\0'; /* off set value first */
+	buf[bytes_read] = '\0'; /* set null terminator */
 
 	close(fd);
 
 	bytes_written =  write(STDOUT_FILENO, buf, bytes_read);
+	/* write to std out of buf based on bytes_read */
 
 	if (bytes_written == -1)
 		return (0);
