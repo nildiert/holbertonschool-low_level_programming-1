c9ffa064599ad864c30a0978fe4db4d4eccb472c
Victor Nguyen <victormdnguyen88@gmail.com>
2018-04-14 22:05:03 +0000

    free everything

---
+++ 0x16-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free a doubly linked list
+ * @head - pointer to the head of the linked list
+ * Return: nothing
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+
+	dlistint_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+}
