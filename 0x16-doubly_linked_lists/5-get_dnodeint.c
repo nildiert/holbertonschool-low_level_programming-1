7e3a471d92b844eb8e512adc2d1b8be6610332d3
Victor Nguyen <victormdnguyen88@gmail.com>
2018-04-14 23:14:32 +0000

    correct return value if index == count

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 /*if the count matches the index then return, else continue */
 		if (count == index)
-			return (count);
+			return (ptr);
 
 		ptr = ptr->next;
 		count++;
