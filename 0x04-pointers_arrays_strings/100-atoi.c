fc78e6614d2010f8a60dd0ea5eab14609210b02b
Victor Nguyen <victormdnguyen88@gmail.com>
2018-04-16 21:10:24 +0000

    atoi

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
  * _atoi - converts a char to an int
@@ -9,9 +10,23 @@
 int _atoi(char *s)
 {
 	int sum = 0;
+	int neg = 0;
+	char *ptr;
+	int flag = 0;
 
 	if (s == NULL)
 		return (-1);
+
+	ptr = s;
+	while (*ptr != '\0')
+	{
+		if (*ptr >= '0' && *ptr <= '9')
+			break;
+
+		if (*ptr == '-')
+			neg++;
+		ptr++;
+	}
 
 	while (*s != '\0')
 	{
@@ -20,11 +35,19 @@ int _atoi(char *s)
 			sum *= 10;
 			sum += (*s - '0');
 			s++;
+			flag = 1;
+
 		}
-
 		else
-			return (-1);
+		{
+			if (flag == 1)
+				break;
+			s++;
+		}
 	}
+	if (neg % 2 == 0)
+		return (sum);
 
-	return (sum);
+	else
+		return (sum * -1);
 }
