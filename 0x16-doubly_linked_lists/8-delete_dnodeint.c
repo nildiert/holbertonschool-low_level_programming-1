28f716e1cf9646311e6d69e7d45673643e92bd31
Victor Nguyen <victormdnguyen88@gmail.com>
2018-04-16 03:43:19 +0000

    add comments

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -17,17 +17,18 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	ptr = *head;
 
-	while (ptr != NULL)
+	while (ptr != NULL) /* count list size */
 	{
 		ptr = ptr->next;
 		count++;
 	}
 
-	if (index >= count)
+	if (index >= count) /* given index is too big so it fails */
 		return (-1);
 
 	ptr = *head;
 
+	/* deletes node if it is at the head & last node of the list */
 	if (index == 0 && count == 1)
 	{
 		free(*head);
@@ -35,17 +36,20 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
+	/* deletes head of list if list has 2 or more nodes */
 	if (index == 0 && count > 1)
 	{
-		*head = ptr->next;
+		ptr = ptr->next;
 		(*head)->prev = NULL;
 		free(ptr);
 		return (1);
 	}
 
+	/* head edge cases accounted for, move pointer to index */
 	for (idx_count = 0; idx_count < index; idx_count++)
 		ptr = ptr->next;
 
+	/* reassigns null and deletes the end node */
 	if (ptr->next == NULL)
 	{
 		ptr->prev->next = NULL;
@@ -53,6 +57,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
+	/* delete the middle node and reassign links */
 	else if (ptr->prev && ptr->next)
 	{
 		ptr->prev->next = ptr->next;
