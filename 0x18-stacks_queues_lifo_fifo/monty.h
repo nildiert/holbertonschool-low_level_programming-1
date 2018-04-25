333c6adfda2cb79033c9b46a8028a70d92776642
Victor Nguyen <victormdnguyen88@gmail.com>
2018-04-25 20:10:57 +0000

    add operations

--- 0x18-stacks_queues_lifo_fifo/monty.h
+++ 0x18-stacks_queues_lifo_fifo/monty.h
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <limits.h>
 
+extern int data;
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -35,5 +37,16 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void op_push(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void div(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
 
 #endif
