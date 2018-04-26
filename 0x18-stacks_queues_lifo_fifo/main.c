2504efa6e199f8f86d6e36d61d63e8d641c4f5de
arturadamian <arturadamian@gmail.com>
2018-04-26 01:36:02 +0000

    fix seg fault error

--- 0x18-stacks_queues_lifo_fifo/main.c
+++ 0x18-stacks_queues_lifo_fifo/main.c
@@ -1,29 +1,40 @@
 #include "monty.h"
 
-int main(int argc char* argv[])
+int data;
+
+int main(int argc, char* argv[])
 {
-	char* buffer[1024];
-	size_t buffsize = 0;
-	FILE *fd = NULL;
-	unsigned int line_number = 0;
+	char *buffer;
+	size_t buffsize;
+	FILE *fd;
+	unsigned int line_number;
 	stack_t **stack = NULL;
+	instruction_t *f;
+	char *token = NULL;
 
+	printf("got here 2?");
 	if (argc != 2)
-		error(argc);
-
-	fd = fopen(argc[1], "r"); /* opens an reads */
-
-	if (fd = NULL)
-		error(fopen);
-
-	while (getline(buffer, buffsize, fd) != -1)
 	{
-		line_number++;
-		token = _strtok(buffer);
-		get_op_func(token);
+		printf("argc is not 2");
+		exit(EXIT_FAILURE);
 	}
 
+	fd = fopen(argv[1], "r"); /* opens an reads */
+	printf("got here 3?");
+	if (fd == NULL)
+		exit(EXIT_FAILURE);
 
+	line_number = 0;
+	buffsize = 0;
+	buffer = NULL;
+	while (getline(&buffer, &buffsize, fd) != -1)
+	{
+		line_number++;
+		token = tokenize(buffer);
+		f = get_op_func(token);
+		f->f(stack, line_number);
+	}
+	return (0);
 }
 
 /**
@@ -31,22 +42,21 @@ int main(int argc char* argv[])
 * Return: tokens
 */
 
-char **strtok(char *s)
+char *tokenize(char *s)
 {
 	char *token; /*str that returns strtok */
 	char *tok_data; /*multi demensional array used to store tokens */
-	int tok = 0;
-	int data;
-	int i = 0;
 
 	token = strtok(s, DELIMITERS);
 
 
 	if (strcmp(token, "push") == 0)
-		tok_data == strtok(NULL, DELIMITERS);
-
-	data = atoi(tok_data);
-
+	{
+		tok_data = strtok('\0', DELIMITERS);
+		if (tok_data)
+			exit(EXIT_FAILURE);
+		data = atoi(tok_data);
+	}
 	get_op_func(token); /* grabs correct op func */
 
 	return (token);
