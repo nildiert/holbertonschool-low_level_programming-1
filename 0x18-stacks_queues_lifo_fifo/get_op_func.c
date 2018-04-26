28f5d643558f3a2fbaa6229a47d78bdb363e0d21
arturadamian <arturadamian@gmail.com>
2018-04-26 01:35:18 +0000

    rearange logic

--- 0x18-stacks_queues_lifo_fifo/get_op_func.c
+++ 0x18-stacks_queues_lifo_fifo/get_op_func.c
@@ -1,9 +1,15 @@
 #include "monty.h"
 
-f get_op_func(const char *s)
+instruction_t *get_op_func(const char *s)
 {
+	int i;
+	instruction_t *opc = NULL;
 	instruction_t ops [] = {
 		{"push", op_push},
+		{NULL, NULL}
+/*
+
+
 		{"pall", op_pall},
 		{"pint", op_pint},
 		{"pop", op_pop},
@@ -15,11 +21,13 @@ f get_op_func(const char *s)
 		{"mul", op_mul},
 		{"mod", op_mod},
 		{NULL, NULL}
+*/
 	};
 
 	i = 0;
 	while (ops[i].opcode && strcmp(ops[i].opcode, s) != 0)
 		i++;
-
-	return (ops[i].f);
+	opc->opcode = ops[i].opcode;
+	opc->f = ops[i].f;
+	return (opc);
 }
