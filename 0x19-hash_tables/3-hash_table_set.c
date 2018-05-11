984693bb80bf78329a60d0d3d2426d388ac395d4
Victor Nguyen <victormdnguyen88@gmail.com>
2018-05-11 17:36:30 +0000

    try strdup

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -18,15 +18,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	newNode = malloc(sizeof(hash_node_t));
+	newNode = malloc(sizeof(hash_node_t*));
 	if (newNode == NULL)
 	{
 		free(newNode);
 		return (0);
 	}
 
-	newNode->value = (char *)value;
-	newNode->key = (char *)key;
+	newNode->value = strdup(value);
+	newNode->key = strdup(key);
 	newNode->next = NULL;
 
 	if (ht->array[index] == NULL)
