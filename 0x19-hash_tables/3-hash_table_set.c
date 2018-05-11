e2bf23453c806f5ace91f7e9f28d83a900886124
Victor Nguyen <victormdnguyen88@gmail.com>
2018-05-11 19:43:45 +0000

    betty

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,9 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *newNode;
 	hash_node_t *ptr;
 
-	if (ht == NULL || key == NULL)
-	    return (0);
-	if (value == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
@@ -25,16 +23,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	while (ptr != NULL)
 	{
 		/* check if key already exists, if so replace it */
-		if(strcmp(key, ptr->key) == 0)
-			{
-				free(ptr->value);
-				ptr->value = strdup(value);
-				return(1);
-			}
+		if (strcmp(key, ptr->key) == 0)
+		{
+			free(ptr->value);
+			ptr->value = strdup(value);
+			return (1);
+		}
 		ptr = ptr->next;
 	}
 
-	newNode = malloc(sizeof(hash_node_t*));
+	newNode = malloc(sizeof(hash_node_t *));
 	if (newNode == NULL)
 		return (0);
 
