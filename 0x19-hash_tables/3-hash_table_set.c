c4eebcffeca8f48ab2253e453c4d6bbea7c34ec0
Victor Nguyen <victormdnguyen88@gmail.com>
2018-05-12 02:46:09 +0000

    malloc for hash_node_t

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -35,7 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	/* allocate newNode */
-	newNode = malloc(sizeof(hash_node_t *));
+	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
 		return (0);
 
