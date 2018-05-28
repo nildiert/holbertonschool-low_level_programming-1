4cb4c3d4024b29da5d890a68679cfbae06166807
vmdn23 <victormdnguyen88@gmail.com>
2018-05-28 01:50:20 +0000

    readd 0 check

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -11,7 +11,7 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j = 0;
 	size_t checker, temp = 0;
 
-	if (array == NULL || size < 2)
+	if (array == NULL || size == 0)
 		return;
 
 	checker = 0;
