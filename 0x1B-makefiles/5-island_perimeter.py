c63e481bbe23b1837cb78183e966c92d286bc85d
vmdn23 <victormdnguyen88@gmail.com>
2018-06-19 00:12:10 +0000

    island perimeter

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Finds the perimeter of an island"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid
+    Args:
+        Grid: A list of integers of 0s (water) and 1s (land)
+    """
+    island_perimeter = 0
+    for col in range(len(grid)):
+        for row in range(len(grid[0])):
+            if grid[col][row] == 1:
+                island_perimeter += 4
+                if row < len(grid[0]) - 1:
+                    if grid[col][row + 1] == 1:
+                        island_perimeter -= 2
+                if col < len(grid) - 1:
+                    if grid[col - 1][row] == 1:
+                        island_perimeter -=2
+    return island_perimeter
