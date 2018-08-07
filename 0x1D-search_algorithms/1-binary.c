a82eda95fb6ce37b0ad996c6fdc5885dc4c3e819
vmdn23 <victormdnguyen88@gmail.com>
2018-08-07 21:36:06 +0000

    iterative binary search

---
+++ 0x1D-search_algorithms/1-binary.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+
+void print_array(int *array, size_t low, size_t high)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = low; i < high; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+}
+
+/**
+ * binary_search - searches for a value in an array of integers
+ * @array: a pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: the value to search for
+ * Return: first index where value is located, else -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t low = 0;
+	size_t high = size - 1;
+
+	while (low <= high)
+	{
+		size_t mid = (high - low) / 2 + low;
+
+		print_array(array, low, high);
+		if (array[mid] == value)
+			return mid;
+		if (value  > array[mid])
+			/* excludes mid */
+			low = mid + 1;
+		else
+			/* excludes mid */
+			high = mid - 1;
+	}
+	return (-1);
+}
